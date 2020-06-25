0982b479097d29de523f8c2625359bbf61098c83
Mario Toribio <toribio.mario@gmail.com>
2020-06-25 22:34:40 +0200

    first commit

---
+++ backend/src/routes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const multer = require("multer");
+const verifyToken = require("./config/verifyToken");
+
+const UserController = require("./controllers/UserController");
+const EventController = require("./controllers/EventController");
+const DashboardController = require("./controllers/DashboardController");
+const LoginController = require("./controllers/LoginController");
+const RegistrationController = require("./controllers/RegistrationController");
+const ApprovalController = require("./controllers/ApprovalController");
+const RejectionController = require("./controllers/RejectionController");
+const uploadConfig = require("./config/upload");
+
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+
+//Router test
+routes.get("/status", (req, res) => {
+    res.send({ status: 200 });
+});
+
+//Registation
+routes.post("/registration/:eventId", verifyToken, RegistrationController.create);
+routes.get("/registration/:registration_id", RegistrationController.getRegistration);
+routes.post("/registration/:registration_id/approvals", verifyToken, ApprovalController.approval);
+routes.post("/registration/:registration_id/rejections", verifyToken, RejectionController.rejection);
+
+//Login
+routes.post("/login", LoginController.store);
+
+//Routes Dashboard
+routes.get("/dashboard/:sport", verifyToken, DashboardController.getAllEvents);
+routes.get("/dashboard", verifyToken, DashboardController.getAllEvents);
+routes.get("/user/events", verifyToken, DashboardController.getEventsByUserId);
+routes.get("/event/:eventId", verifyToken, DashboardController.getEventById);
+
+//Routes Event
+routes.post("/event", verifyToken, upload.single("thumbnail"), EventController.createEvent);
+routes.delete("/event/:eventId", verifyToken, EventController.delete);
+
+//Routes User
+routes.post("/user/register", UserController.createUser);
+routes.get("/user/:userId", UserController.getUserById);
+
+module.exports = routes;
